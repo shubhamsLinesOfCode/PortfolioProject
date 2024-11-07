eda5444d2218bf23cbc24e4da168145be2fe9fff
Shubham Paul <iamshubhham@hotmail.com>
2024-11-08 03:48:09 +0530

    Gsap animations to the mobile side nav menu

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Check for the user's saved theme on page load
-/* if (localStorage.theme === "dark") {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
-
-// Function to toggle theme and save it in localStorage
-function toggleTheme() {
-  document.documentElement.classList.toggle("dark");
-
-  if (document.documentElement.classList.contains("dark")) {
-    localStorage.theme = "dark";
-  } else {
-    localStorage.theme = "light";
-  }
-} */
-
 function setThemeFromPreference() {
   const isDarkMode =
     localStorage.theme === "dark" ||
@@ -41,15 +23,25 @@ window
 
 // Apply the initial theme on page load
 setThemeFromPreference();
-// Attach toggleTheme to a button click event
-//   document.getElementById("theme-toggle-button").addEventListener("click", toggleTheme);
+/*  Attach toggleTheme to a button click event
+  document.getElementById("theme-toggle-button").addEventListener("click", toggleTheme); */
 const sideMenu = document.querySelector("#sideMenu");
-
 function openMenu() {
   sideMenu.style.transform = "translateX(-16rem)";
+
+  gsap.set("#sideMenu li a", { opacity: 0 });
+  gsap.to("#sideMenu li a", {
+    display: "block",
+    x: 50,
+    opacity: 1,
+    duration: 1.15,
+    delay: 1,
+    stagger: 0.4,
+  });
 }
 function closeMenu() {
   sideMenu.style.transform = "translateX(16rem)";
+  gsap.set("#sideMenu li a", { display: "inline", x: 0 });
 }
 
 // Show button after scrolling down a bit
